05e6a40230bf889611c09342ed106ef471018909
Amaldev <amaldev.psn@gmail.com>
2023-01-09 18:57:20 +0530

    136

--- algorithms/136. Single Number/usingXor.js
+++ algorithms/136. Single Number/usingXor.js
@@ -1,5 +1,4 @@
-function singleNumber(nums: number[]): number {
-    let ms = new Map();
+function singleNumber(nums) {
     let num = 0;
     for (let i = 0; i < nums.length; i++) {
         num = num ^ nums[i];
