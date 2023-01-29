3c99b1707014e17592d2c0a9c80db0616d27784e
Amaldev <amaldev.psn@gmail.com>
2023-01-29 12:57:20 +0530

    113

---
+++ algorithms/Tree/113. Path Sum II/index.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function pathSum(root, targetSum) {
+    let ar = [];
+    let out = [];
+    dfs(root, 0, ar, out);
+    return out;
+};
+function dfs(node, sum, targetSum) {
+    if (node.left == null && node.right == null) {
+        sum = node.val + sum;
+        ar.push(node.val);
+        if (sum == targetSum) {
+            out.push(ar)
+        }
+    }
+    sum = sum + node.val;
+    sum = node.val + sum;
+    ar.push(node.val);
+    if (node.left) dfs(node.left, sum, targetSum);
+    if (node.right) dfs(node.right, sum, targetSum);
+}
