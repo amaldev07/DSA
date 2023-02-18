943b678ae406f4b726cb8bed416b452c7bb5fda0
Amaldev <>
2023-02-19 03:08:36 +0530

    aa

--- recursion/Kunal/arraySubsetWithDuplicate.js
+++ recursion/Kunal/arraySubsetWithDuplicate.js
@@ -1,3 +1,27 @@
+// Correct method
+function arraySubsets1(arr) {
+    debugger;
+    let outer = [];
+    outer.push([]);
+    let start = 0;
+    let end = 0;
+    for (let i = 0; i < arr.length; i++) {
+        start = 0;
+        if (i > 0 && arr[i] == arr[i - 1]) {
+            start = end + 1;
+        }
+        end = outer.length - 1;
+        // let newOuter = JSON.parse(JSON.stringify(outer));
+        for (let j = start; j < (end + 1); j++) {
+            let newItem = [...outer[j]];
+            newItem.push(arr[i]);
+            outer.push(newItem);
+        }
+    }
+    return outer;
+}
+
+//////////////////////
 function arraySubsets(arr) {
     debugger;
     let outer = [];
