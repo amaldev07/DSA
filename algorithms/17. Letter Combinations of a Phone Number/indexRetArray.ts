bd14de56955cd006cfbf53d3866cf794729fdc28
Amaldev <amaldev.psn@gmail.com>
2023-02-21 01:47:09 +0530

    lc-17 practices

---
+++ algorithms/17. Letter Combinations of a Phone Number/indexRetArray.ts
@@ -0,0 +1,14 @@
+function dPadRetA(p, up): any {
+    if (up == '') {
+        return [p];
+    }
+    let digit = +up.charAt(0);
+    let start = (digit - 1) * 3;
+    let end = (digit) * 3;
+    let arr = [];
+    for (let i = start; i < end; i++) {
+        let newCh = String.fromCharCode(+('a'.charCodeAt(0)) + i);
+        arr = arr.concat(dPadRetA(p + newCh, up.substring(1)));
+    }
+    return arr;
+}
