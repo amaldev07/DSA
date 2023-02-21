53d1ddaad1339faa25816dbeb5fbbde97f2f4caa
Amaldev <amaldev.psn@gmail.com>
2023-02-22 00:22:06 +0530

    234. Palindrome Linked List

--- algorithms/1155. Number of Dice Rolls With Target Sum/lc.js
+++ algorithms/1155. Number of Dice Rolls With Target Sum/lc.js
@@ -1,15 +1,15 @@
 function numRollsToTarget(n, k, target) {
     debugger;
-    let lenAr = recFaces('', target, k, n);
-    return lenAr.length;
+    let count = recFaces('', target, k, n, 0);
+    return count;
 };
 
 
-function recFaces(p, up, faces, n) {
+function recFaces(p, up, faces, n, count) {
     debugger;
     //p here is string
     if (up == 0) {
-        return [p];
+        return count+1;
     }
     // if (p.length >= n) {
     //     return [];
@@ -18,10 +18,10 @@ function recFaces(p, up, faces, n) {
     for (let i = 0; i < (up + 1) && i < faces; i++) {
         let newP = p + (i + 1)
         if ((up - (i + 1)) == 0 && newP.length <= n) {
-            ret = ret.concat(recFaces(newP, up - (i + 1), faces, n));
+            count = count + (recFaces(newP, up - (i + 1), faces, n,0));
         } else if (newP.length < n) {
-            ret = ret.concat(recFaces(newP, up - (i + 1), faces, n));
+            count = count + (recFaces(newP, up - (i + 1), faces, n,0));
         }
     }
-    return ret;
+    return count;
 }
