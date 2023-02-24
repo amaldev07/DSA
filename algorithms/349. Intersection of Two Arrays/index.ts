7e37bd38ac950ae7612c385bcb980c7d167aee13
Amaldev <>
2023-02-25 00:59:14 +0530

    349. Intersection of Two Arrays

---
+++ algorithms/349. Intersection of Two Arrays/index.ts
@@ -0,0 +1,13 @@
+function intersection(nums1: number[], nums2: number[]): number[] {
+    let intersec: number[] = [];
+    let num1Set = new Set();
+    for (let i = 0; i < nums1.length; i++) {
+        num1Set.add(nums1[i]);
+    }
+    for (let i = 0; i < nums2.length; i++) {
+        if (num1Set.has(nums2[i]) && !intersec.includes(nums2[i])) {
+            intersec.push(nums2[i]);
+        }
+    }
+    return intersec;
+};
