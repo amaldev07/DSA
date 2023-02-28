45cb12caad58556f0ae0535624e73cced72081ea
Amaldev <amaldev.psn@gmail.com>
2023-03-01 01:41:25 +0530

    350. Intersection of Two Arrays II

---
+++ algorithms/350. Intersection of Two Arrays II/index.ts
@@ -0,0 +1,19 @@
+/* 
+https://leetcode.com/problems/intersection-of-two-arrays/description/?orderBy=most_relevant
+https://www.youtube.com/watch?v=w5mBjxL1pok
+ */
+
+
+function intersect(nums1: number[], nums2: number[]): number[] {
+    let intersec: number[] = [];
+    let num1Set = new Set();
+    for (let i = 0; i < nums1.length; i++) {
+        num1Set.add(nums1[i]);
+    }
+    for (let i = 0; i < nums2.length; i++) {
+        if (num1Set.has(nums2[i])) {
+            intersec.push(nums2[i]);
+        }
+    }
+    return intersec;
+};
