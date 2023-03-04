92a59df3c7d9a977419ed6b7897115a897cce11d
Amaldev <amaldev.psn@gmail.com>
2023-03-04 22:06:01 +0530

    1342. Number of Steps to Reduce a Number to Zero

---
+++ algorithms/1342. Number of Steps to Reduce a Number to Zero/index.ts
@@ -0,0 +1,12 @@
+function numberOfSteps(num: number): number {
+    let count = 0;
+    while (num > 0) {
+        if (num % 2 == 0) {
+            num = num / 2;
+        } else {
+            num = num - 1;
+        }
+        count = count + 1;
+    }
+    return count;
+};
