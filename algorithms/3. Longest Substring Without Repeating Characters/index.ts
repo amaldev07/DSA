ea21f3d28949e2aa327ca0663d1378bdfe9189a8
Amaldev <amaldev.psn@gmail.com>
2023-03-18 02:33:27 +0530

    3. Longest Substring Without Repeating Characters

---
+++ algorithms/3. Longest Substring Without Repeating Characters/index.ts
@@ -0,0 +1,14 @@
+function lengthOfLongestSubstring(s: string): number {
+    let l = 0;
+    let mySet = [];
+    let max = 0;
+    for (let i = 0; i < s.length; i++) {
+        while (mySet.includes(s[i])) {
+            mySet.shift();
+            l = l + 1;
+        }
+        mySet.push(s[i]);
+        max = Math.max(max, (i - l) + 1);
+    }
+    return max;
+};
