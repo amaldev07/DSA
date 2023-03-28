515c0b8979d3bd292c4ea5ef227ecf71f6f4f438
Amaldev <amaldev.psn@gmail.com>
2023-03-28 12:27:20 +0530

    c

--- algorithms/56. Merge Intervals/index.js
+++ algorithms/56. Merge Intervals/index.js
@@ -1,3 +1,19 @@
 function merge(intervals) {
+    // [[1,3],[2,6],[8,10],[15,18]]
+    let out = [];
+    let start = 0;
+    let end = 0;
+    for (let i = 0; i < intervals.length; i++) {
+        let el = intervals[i];
+        if (start < el[0] && end < el[0]) {
+            start = el[0];
+            end = el[1];
+        } else {
+            start = Math.min(el[0], start);
+            end = Math.max(el[0], end);
+        }
+        out.push([start, end]);
+    }
+    return out;
 
 };
