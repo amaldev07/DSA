437f8bedd204639146f29ef3147dd4fc1bfe737b
Amaldev <amaldev.psn@gmail.com>
2023-04-02 18:27:20 +0530

    c

--- algorithms/2798. Number of Employees Who Met the Target/index.js
+++ algorithms/2798. Number of Employees Who Met the Target/index.js
@@ -3,21 +3,20 @@ function numberOfEmployeesWhoMetTarget(hours, target) {
     let e = hours.length - 1;
     let count = 0;
     while (s <= e) {
-      if (s == e) {
-        if (hours[s] >= target) {
-          count = count + 1;
+        if (s == e) {
+            if (hours[s] >= target) {
+                count = count + 1;
+            }
+        } else {
+            if (hours[s] >= target) {
+                count = count + 1;
+            }
+            if (hours[e] >= target) {
+                count = count + 1;
+            }
         }
-      } else {
-        if (hours[s] >= target) {
-          count = count + 1;
-        }
-        if (hours[e] >= target) {
-          count = count + 1;
-        }
-      }
-  
-      s = s + 1;
-      e = e - 1;
+        s = s + 1;
+        e = e - 1;
     }
     return count;
-  };
+};
