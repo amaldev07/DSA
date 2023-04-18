2293b01e1af9abb4649632652db82ad9f905fa69
Amal <amaldev.psn@gmail.com>
2023-04-18 20:12:20 +0530

    c

--- algorithms/53. Maximum Subarray/index.js
+++ algorithms/53. Maximum Subarray/index.js
@@ -1,7 +1,7 @@
 function maxSubArray(nums) {
     let cSum = nums[0];
     let max = nums[0];
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = 1; i < nums.length; i++) {
         if (cSum <= 0) cSum = 0;
         cSum = cSum + nums[i];
         max = Math.max(cSum, max);
