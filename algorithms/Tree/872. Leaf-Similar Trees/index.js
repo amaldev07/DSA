4d44bd639bd51c6bea967ba3b9526e858cf1265d
Amaldev <amaldev.psn@gmail.com>
2023-05-05 02:57:20 +0530

    c

---
+++ algorithms/Tree/872. Leaf-Similar Trees/index.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function leafSimilar(root1: TreeNode | null, root2: TreeNode | null): boolean {
+    let out = [];
+    dfs(root1, out, false);
+    console.log(out)
+    let res =  dfs(root2, out, true);
+    if(out.length>0) return false;
+    return res;
+};
+function dfs(node, out, flag) {
+    if (node == null) return;
+    if (node.left == null && node.right == null) {
+        if (flag == false) {
+            console.log(node.val)
+            out.push(node.val);
+        } else {
+            console.log(out, node.val)
+            if (out.length <= 0) return false;
+            let el = out.shift();
+            if (el !== node.val) return false;
+            return true;
+        }
+    }
+    if (node.left == null) {
+        let r = dfs(node.right, out, flag);
+        return r;
+    } else if (node.right == null) {
+        let l = dfs(node.left, out, flag);
+        return l;
+    } else {
+        let l = dfs(node.left, out, flag);
+        let r = dfs(node.right, out, flag);
+        return l && r;
+    }
+}
