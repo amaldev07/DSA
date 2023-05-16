eaa2c8f2f3502fbf902cc192020d64ac1f506fc3
Amaldev <amaldev.psn@gmail.com>
2023-05-17 01:53:20 +0530

    fibonacciRecursionMemoization

--- algorithms/Dynamic programming/fibonacciRecursionMemoization.js
+++ algorithms/Dynamic programming/fibonacciRecursionMemoization.js
@@ -1,12 +1,16 @@
-function main() {
-    let n = 5;
+function main(n) {
     let dp = new Array(n + 1).fill(-1);
     return fibonacci(n, dp);
+    /*  let fib = fibonacci(n, dp);
+     console.log(dp);
+     return fib */
 }
 function fibonacci(n, dp) {
-    if (n <= 1) return 1;
+    if (n <= 1) return n;
     if (dp[n] != -1) return dp[n];
     let fSum = fibonacci(n - 1, dp) + fibonacci(n - 2, dp);
     dp[n] = fSum;
     return fSum;
 }
+// main(5);
+// fibonacciRecursionMemoization
