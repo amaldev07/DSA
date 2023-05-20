a9997f28c6ca2a0432e8b70eb1e40f7129691785
Amaldev <amaldev.psn@gmail.com>
2023-05-20 13:15:32 +0530

    Fibonacci tabulation

---
+++ algorithms/Dynamic programming/fibonacciTabulation.js
@@ -0,0 +1,9 @@
+function fibonacciTabulation(n) {
+    let dp = new Array(n + 1).fill(-1);
+    dp[0] = 0;
+    dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
