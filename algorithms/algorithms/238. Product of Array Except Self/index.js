33df361f242193f64da7a5764436fdbb81d2292d
Amaldev <amaldev.psn@gmail.com>
2023-06-13 12:00:20 +0530

    c

--- algorithms/algorithms/238. Product of Array Except Self/index.js
+++ algorithms/algorithms/238. Product of Array Except Self/index.js
@@ -1,3 +1,14 @@
 function productExceptSelf(nums: number[]): number[] {
-
+    let res = [];
+    let prefix = 1;
+    for (let i = 0; i < nums.length; i++) {
+        res = prefix;
+        prefix = prefix * nums[i];
+    }
+    prefix =1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        res = prefix*res[i];
+        prefix = prefix * nums[i];
+    }
+    return res;
 };
