c67055afc9adec510c8eb6c3f5d7a156686d69ab
Amaldev <>
2023-07-09 00:56:16 +0530

    102. Binary Tree Level Order Traversal

---
+++ algorithms/Tree/levelOrderTraversal.js
@@ -0,0 +1,15 @@
+function levelOrderTraversal(root) {
+    let out = [] // [[1],[2,3],[4,5,6,7]]
+    let queue = [];
+    queue.push(root);
+    while (queue.length > 0) {
+        let tempout = [];
+        for (let i = 0; i < queue.length; i++) {
+            let el = queue.shift();
+            tempout.push(el.val);
+            if (el.left) queue.push(el.left);
+            if (el.right) queue.push(el.right);
+        }
+        out.push(tempout);
+    }
+}
