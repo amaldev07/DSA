d18eaa6d9bf1b860eb8a5fe1a823ee32a00cb276
Amaldev <amaldev.psn@gmail.com>
2023-07-23 08:17:20 +0530

    202. Happy Number

--- algorithms/202. Happy Number/index.js
+++ algorithms/202. Happy Number/index.js
@@ -1,3 +1,22 @@
-function isHappy(n: number): boolean {
+function isHappy(n) {
+    debugger;
+    let m = new Map();
+    m.set(n, 1);
+    while (n != 1) {
+        let temp = n;
+        let sum = 0;
+        while (temp != 0) {
+            sum = sum + Math.pow(temp % 10, 2);
+            temp = Math.floor(temp / 10);
+        }
+        n = sum;
+        if (m.get(temp) >= 1) {
+            return false;
+        } else {
+            m.set(n, 1);
+        }
 
+    }
+    return true;
 };
+isHappy(19);
