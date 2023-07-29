d353fcbdcfdceae5bb57ab4620c6dcaf422bcdf7
Amaldev <amaldev.psn@gmail.com>
2023-07-30 01:00:00 +0530

    545. Boundary of Binary Tree

--- algorithms/Tree/545. Boundary of Binary Tree/index.js
+++ algorithms/Tree/545. Boundary of Binary Tree/index.js
@@ -11,5 +11,48 @@
  * @return {number[]}
  */
 var boundaryOfBinaryTree = function (root) {
-let ans = []
+    let ans = [];
+    if (!isLeaf(root)) ans.push(root.val);
+    addLeftBoundry(root, ans);
+    addLeafNodes(root, ans);
+    addrightBoundry(root, ans);
+    return ans;
 };
+function isLeaf(node) {
+    if (!node.left && !node.right) return true;
+    return false;
+}
+function addLeftBoundry(node, ans) {
+    let cur = node.left;
+    while (cur) {
+        if (!isLeaf(cur)) {
+            ans.push(cur.val);
+            if (cur.left) cur = cur.left;
+            else cur = cur.right;
+        } else {
+            cur = null;
+        }
+    }
+}
+function addLeafNodes(node, ans) {
+    if (isLeaf(node)) ans.push(node.val);
+    if (node.left) addLeafNodes(node.left, ans);
+    if (node.right) addLeafNodes(node.right, ans);
+}
+
+function addrightBoundry(node, ans) {
+    let rightAns = []
+    let cur = node.right;
+    while (cur) {
+        if (!isLeaf(cur)) {
+            rightAns.push(cur.val);
+            if (cur.right) cur = cur.right;
+            else cur = cur.left;
+        } else {
+            cur = null;
+        }
+    }
+    for (let i = rightAns.length - 1; i >= 0; i--) {
+        ans.push(rightAns[i])
+    }
+}
