ed6e50ab6fdbd1517dc98a588cdec684908d49cd
Amaldev <amaldev.psn@gmail.com>
2023-08-14 01:38:20 +0530

    preOrderIterative

--- algorithms/Tree/preOrderTraversalIterative.js
+++ algorithms/Tree/preOrderTraversalIterative.js
@@ -1,11 +1,12 @@
 function preOrderIterative(root) {
+    if (root == null) return [];
     let s = [root];
     let out = [];
     while (s.length > 0) {
         let lastEl = s.pop(); // see the https://github.com/amaldev07/js_array_functions for array.pop()
         out.push(lastEl.val);
-        if (root.right) s.push(root.right);
-        if (root.left) s.push(root.left);
+        if (lastEl.right) s.push(lastEl.right);
+        if (lastEl.left) s.push(lastEl.left);
     }
     return out;
 }
