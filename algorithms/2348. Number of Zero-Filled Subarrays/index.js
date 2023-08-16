b3899a5e48a064596f9c66d811d99cc6e46c2b56
Amaldev <amaldev.psn@gmail.com>
2023-08-16 10:33:47 +0530

    c

---
+++ algorithms/2348. Number of Zero-Filled Subarrays/index.js
@@ -0,0 +1,18 @@
+// [1,3,0,0,2,0,0,4]
+function zeroFilledSubarray(nums) {
+    let count = 0
+    let permuSum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 0) {
+            count = count + 1;
+        } else {
+            permuSum = permuSum + permutation(count);
+            count = 0;
+        }
+        permuSum = permuSum + permutation(count);
+    }
+    return permuSum;
+};
+function permutation(n) {
+    return (n)(n + 1) / 2;
+}
