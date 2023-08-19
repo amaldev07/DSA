dc98a5557f4bc92ce4d7419c061cf5c071d51322
Amaldev <amaldev.psn@gmail.com>
2023-08-20 02:05:40 +0530

    c

--- algorithms/11. Container With Most Water/index.js
+++ algorithms/11. Container With Most Water/index.js
@@ -2,7 +2,18 @@
 start moving i  and j in such a way that we wnat oto keep the most height
 if the ext one in left is small then  move right to one before */
 function maxArea(height) {
-
+    let maxArea = 0;
+    let i = 0;
+    let j = height.length - 1;
+    while (i < j) {
+        maxArea = Math.max(maxArea, getArea(height[i], height[j], (j - i)));
+        if (height[i + 1] > height[j - 1]) {
+            i = i + 1;
+        } else {
+            j = j - 1;
+        }
+    }
+    return maxArea;
 };
 function getArea(x, y, w) {
     let min = Math.min(x, y);
