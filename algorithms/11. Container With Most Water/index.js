4a1c02e8cefa0a1add46f799be83441f32282588
Amaldev <amaldev.psn@gmail.com>
2023-08-20 02:02:40 +0530

    c

--- algorithms/11. Container With Most Water/index.js
+++ algorithms/11. Container With Most Water/index.js
@@ -1,3 +1,6 @@
+/* use 2 PointerEventstart i from 0 and j from len-1
+start moving i  and j in such a way that we wnat oto keep the most height
+if the ext one in left is small then  move right to one before */
 function maxArea(height) {
 
 };
