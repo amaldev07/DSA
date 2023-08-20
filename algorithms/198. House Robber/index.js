4703338995e2b4a4fa9fc63d615682b665bd2860
Amaldev <amaldev.psn@gmail.com>
2023-08-21 01:35:41 +0530

    c

--- algorithms/198. House Robber/index.js
+++ algorithms/198. House Robber/index.js
@@ -1,3 +1,12 @@
 function rob(nums) {
-
+    // [2,7,9,3,1]
+    let rob1 = 0; //2 
+    let rob2 = 0; //7
+    let decision = 0;
+    for (let i = 0; i < nums; i++) {
+        decision = Math.max(nums[i] + rob1, rob2);
+        rob1 = rob2;
+        rob2 = decision;
+    }
+    return decision;
 };
