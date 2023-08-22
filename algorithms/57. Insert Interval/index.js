c5ad8263d34bc22de4dbd6098c9fd29d73ebf105
Amal <amaldev.psn@gmail.com>
2023-08-23 00:59:54 +0530

    c

--- algorithms/57. Insert Interval/index.js
+++ algorithms/57. Insert Interval/index.js
@@ -1,19 +1,17 @@
 function insert(intervals, newInterval) {
     // intervals = [[1,3],[6,9]], newInterval = [4,5]
+    let res = [];
     for (let i = 0; i < intervals.length; i++) {
         if (newInterval[1] < intervals[i][0]) {
-            //  push to left
-            newInterval.splice(i, newInterval);
-            break;
+            res.push(newInterval);
+            return [...res, intervals.slice(i)]
         } else if ((newInterval[0] > intervals[i][1])) {
-            // push to righ
-            newInterval.splice(i+1, newInterval);
-            break;
+            res.push(intervals[i])
         } else {
-            //  merge
             intervals[i][0] = Math.min(intervals[i][0], newInterval[0]);
             intervals[i][1] = Math.min(intervals[i][1], newInterval[1]);
-            break;
         }
     }
+    res.push(newInterval);
+    return res;
 };
