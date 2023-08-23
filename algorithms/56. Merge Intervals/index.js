c56327b0bbcbc573a665fe4e31809eb2f15cb9e7
Amal <amaldev.psn@gmail.com>
2023-08-24 01:04:02 +0530

    c

--- algorithms/56. Merge Intervals/index.js
+++ algorithms/56. Merge Intervals/index.js
@@ -1,9 +1,7 @@
 function merge(intervals) {
-    debugger;
     let i = 0;
-    let c = 0;
     let len = intervals.length;
-    let res= [];
+    let res = [];
     intervals = intervals.sort((a, b) => b[0] > a[0] ? -1 : 1)
     while (i < len - 1) {
         let f = intervals[i];
@@ -11,15 +9,12 @@ function merge(intervals) {
         if (f[1] >= s[0] || s[1] <= f[1]) {
             s[0] = Math.min(f[0], s[0]);
             s[1] = Math.max(f[1], s[1]);
-            c = c + 1;
-            i = i + 1;
         } else {
             res.push(intervals[i]);
-            i = i + 1;
         }
+        i = i + 1;
     }
-     res.push(intervals[len-1]);
-     return res;
+    res.push(intervals[len - 1]);
+    return res;
 
 };
-merge([[2, 3], [2, 2], [3, 3], [1, 3], [5, 7], [2, 2], [4, 6]]);
