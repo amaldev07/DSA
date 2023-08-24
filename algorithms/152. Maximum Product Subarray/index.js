258866f36f5b642b794cbf62654301e719c410eb
Amaldev <amaldev.psn@gmail.com>
2023-08-25 01:35:26 +0530

    c

--- algorithms/152. Maximum Product Subarray/index.js
+++ algorithms/152. Maximum Product Subarray/index.js
@@ -1,10 +1,12 @@
 function maxProduct(nums) {
     let cMin = 1;
     let cMax = 1;
+    let max = nums[0];
     for (let i = 0; i < nums.length; i++) {
         let tempMin = cMin;
         cMin = Math.min(cMax * nums[i], cMin * nums[i], nums[i])
         cMax = Math.max(cMax * nums[i], tempMin * nums[i], nums[i])
+        max = Math.max(max, cMax);
     }
-    return Math.max(cMin, cMax);
+    return max;
 };
