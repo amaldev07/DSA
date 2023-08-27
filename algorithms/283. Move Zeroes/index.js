2b49a8910aa2e8837babbc2e18818b3946352f92
Amaldev <amaldev.psn@gmail.com>
2023-08-27 11:10:07 +0530

    c

--- algorithms/283. Move Zeroes/index.js
+++ algorithms/283. Move Zeroes/index.js
@@ -1,6 +1,15 @@
 /**
  Do not return anything, modify nums in-place instead.
  */
-function moveZeroes(nums: number[]): void {
-
+function moveZeroes(nums) {
+    let pos = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+            if (pos != i) {
+                nums[pos] = nums[i];
+                nums[i] = 0;
+            }
+            pos = pos + 1;
+        }
+    }
 };
