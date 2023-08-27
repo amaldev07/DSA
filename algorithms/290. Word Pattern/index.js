9fffe0404c8e7f7a422334d5d1ed98d95d7c5cb9
Amaldev <amaldev.psn@gmail.com>
2023-08-27 13:10:59 +0530

    c

--- algorithms/290. Word Pattern/index.js
+++ algorithms/290. Word Pattern/index.js
@@ -1,3 +1,14 @@
 function wordPattern(pattern, s) {
-
+    let hm = new Map();
+    let sAry = s.split(" ");
+    for (let i = 0; i < sAry.length; i++) {
+        let letter = pattern[i];
+        if (hm.get(letter) == null) {
+            hm.set(letter, sAry[i])
+        } else {
+            let hmItem = hm.get(letter);
+            if (hmItem != sAry[i]) return false;
+        }
+    }
+    return true;
 };
