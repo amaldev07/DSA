7350d25e4c985a6ece8d5a5b0820e568018cac92
Amaldev <amaldev.psn@gmail.com>
2023-08-27 13:30:52 +0530

    c

--- algorithms/290. Word Pattern/index.js
+++ algorithms/290. Word Pattern/index.js
@@ -1,20 +1,18 @@
 function wordPattern(pattern, s) {
-    debugger;
     let hm = new Map();
     let whm = new Map();
     let sAry = s.split(" ");
-    if(pattern.length!=sAry.length) return false;
+    if (pattern.length != sAry.length) return false;
     for (let i = 0; i < sAry.length; i++) {
-      let letter = pattern[i];
-      if (hm.get(letter) == null) {
-        hm.set(letter, sAry[i])
-        if (whm.get(sAry[i])!=null) return false;
-          whm.set(sAry[i], letter)
-      } else {
-        let hmItem = hm.get(letter);
-        if (hmItem != sAry[i]) return false;
-      }
+        let letter = pattern[i];
+        if (hm.get(letter) == null) {
+            hm.set(letter, sAry[i])
+            if (whm.get(sAry[i]) != null) return false;
+            whm.set(sAry[i], letter)
+        } else {
+            let hmItem = hm.get(letter);
+            if (hmItem != sAry[i]) return false;
+        }
     }
     return true;
-  };
-  wordPattern("a", "dog");
+};
