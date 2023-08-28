83d7b21ac9164ca6197f83b5897608aaac9ca35f
Amaldev <amaldev.psn@gmail.com>
2023-08-29 01:19:08 +0530

    c

--- algorithms/Tree/671. Second Minimum Node In a Binary Tree/index.js
+++ algorithms/Tree/671. Second Minimum Node In a Binary Tree/index.js
@@ -21,7 +21,7 @@ function bfs(node) {
     let l = node.left.val;
     let r = node.right.val;
     if (node.val == l) l = bfs(node.l);
-    if (node.val == r) r = bfs(node.l);
+    if (node.val == r) r = bfs(node.r);
     if (l != -1 && r != -1) {
         return Math.max(l, r)
     } else if (l == -1) {
