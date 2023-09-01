4ef6f745179d80d4dc138d90ab11be299d8a3665
Amaldev <amaldev.psn@gmail.com>
2023-09-02 00:45:33 +0530

    c

---
+++ algorithms/535. Encode and Decode TinyURL/index.js
@@ -0,0 +1,26 @@
+/**
+ * Encodes a URL to a shortened URL.
+ */
+let baseUrl = "https://tinyUrl.com/"
+let hm = new Map();
+function encode(longUrl: string): string {
+    let encyUrl = fromCharCode(Math.random() * 100);
+    while (hm.get(encyUrl) != null) {
+        encyUrl = encyUrl + fromCharCode(Math.random() * 100);
+    }
+    hm.set(longUrl, encyUrl)
+    return baseUrl + encyUrl;
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ */
+function decode(shortUrl: string): string {
+    let origin = (new URL(shortUrl)).origin;
+    return hm.get(shortUrl.split(baseUrl)[1]);
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(strs));
+ */
