12d59a27df5d5f2d43bb113c7ba09b62abb1c7b6
Amaldev <amaldev.psn@gmail.com>
2023-09-04 01:32:34 +0530

    c

--- algorithms/Tree/108. Convert Sorted Array to Binary Search Tree/index.js
+++ algorithms/Tree/108. Convert Sorted Array to Binary Search Tree/index.js
@@ -1,1 +1,25 @@
-108. Convert Sorted Array to Binary Search Tree
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function sortedArrayToBST(nums: number[]): TreeNode | null {
+    return maketree(nums, 0, nums.length - 1);
+};
+function maketree(nums, s, e) {
+    if (e > s) return null;
+    m = Math.floor(s + (e - s) / 2);
+    let root = new TreeNode(nums[m]);
+    root.left = maketree(nums, s, m - 1);
+    root.right = maketree(nums, m + 1, e);
+    return root;
+}
