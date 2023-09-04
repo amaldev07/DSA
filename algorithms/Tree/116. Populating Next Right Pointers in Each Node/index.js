07e62a8856524cdfe5fac12f324f995f5a3463d5
Amaldev <amaldev.psn@gmail.com>
2023-09-04 14:24:52 +0530

    c

--- algorithms/Tree/116. Populating Next Right Pointers in Each Node/index.js
+++ algorithms/Tree/116. Populating Next Right Pointers in Each Node/index.js
@@ -15,27 +15,30 @@
  */
 
 function connect(root: Node | null): Node | null {
-    let out = [] // [[1],[2,3],[4,5,6,7]]
+    /* let out = [] // [[1],[2,3],[4,5,6,7]] */
     let queue = [];
-    if(root==null) return root;
+    if (root == null) return root;
     queue.push(root);
     while (queue.length > 0) {
         let tempout = [];
         let len = queue.length;
-        for (let i = 0; i <len ; i++) {
+        for (let i = 0; i < len; i++) {
             let el = queue.shift();
             tempout.push(el);
             if (el.left) queue.push(el.left);
             if (el.right) queue.push(el.right);
         }
-        out.push(tempout);
-    }
-    console.log(out)
-    for (let i = 0; i < out.length; i++) {
-            let el = out[i];
-            for(let j = 0;j<el.length;j++){
-                el[j].next =(el[j+1]) ? el[j+1]:null;
-            }
+        /* out.push(tempout); */
+        let el = tempout;
+        for (let j = 0; j < el.length; j++) {
+            el[j].next = (el[j + 1]) ? el[j + 1] : null;
         }
-        return root;
+    }
+   /*  for (let i = 0; i < out.length; i++) {
+        let el = out[i];
+        for (let j = 0; j < el.length; j++) {
+            el[j].next = (el[j + 1]) ? el[j + 1] : null;
+        }
+    } */
+    return root;
 };
