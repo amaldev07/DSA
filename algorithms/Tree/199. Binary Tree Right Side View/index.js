abe26492f880cf5e92574dfdd227e2c1d8a72c6a
Amaldev <amaldev.psn@gmail.com>
2023-09-09 01:10:22 +0530

    c

---
+++ algorithms/Tree/199. Binary Tree Right Side View/index.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function rightSideView(root: TreeNode | null): number[] {
+    let q = [root];
+    let out = [];
+    while (q.length > 0) {
+        let len = q.length;
+        out.push(q[q.len - 1]);
+        for (let i = 0; i < len; i++) {
+            let el = q.shift();
+            if (el.left) q.push(el.left);
+            if (el.right) q.push(el.right);
+        }
+    }
+    return out;
+}; 
