ac323ddedae66bb932ba089625c893e47bf93709
Amaldev <amaldev.psn@gmail.com>
2023-09-09 00:11:25 +0530

    c

--- algorithms/Tree/530. Minimum Absolute Difference in BST/index.js
+++ algorithms/Tree/530. Minimum Absolute Difference in BST/index.js
@@ -13,18 +13,14 @@
  */
 
 function getMinimumDifference(root: TreeNode | null): number {
-    let min = [Number.MAX_VALUE];
-    dfs(root, min);
-    return min[0]
+    let g = { prev: Number.MAX_VALUE, min: Number.MAX_VALUE };
+    dfs(root, g);
+    return g.min;
 };
-function dfs(node, min) {
-    if (node == null) return;
-    if (node.left == null && node.right == null) return;
-    let l = Number.MAX_VALUE;;
-    let r = Number.MAX_VALUE;;
-    if (node.left) l = Math.abs(node.val - node.left.val);
-    if (node.right) r = Math.abs(node.val - node.right.val);
-    min[0] = Math.min(min[0], l, r);
-    if (node.left) dfs(node.left, min);
-    if (node.right) dfs(node.right, min);
+function dfs(node, g) {
+    if (node.left != null) dfs(node.left, g);
+    g.min = Math.min(g.min, Math.abs(g.prev - node.val));
+    g.prev = node.val;
+    if (node.right != null) dfs(node.right, g);
+
 }
