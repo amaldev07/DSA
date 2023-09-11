5801aa9e6d6e8a1c29dfd51bab9893562f50d54d
Amaldev <amaldev.psn@gmail.com>
2023-09-11 12:09:50 +0530

    c

--- algorithms/index.js
+++ algorithms/index.js
@@ -1,30 +1,17 @@
-function addBinary(a: string, b: string): string {
-    let aAr = a.split('');
-    let bAr = b.split('');
-    let len = aAr.length > bAr.length ? aAr.length : bAr.length;
-    let res = '';
+function addBinary(a, b) {
+    debugger;
+    let i = a.length - 1;
+    let j = b.length - 1;
     let c = 0;
-    for (let i = 0; i < len; i++) {
-        let indOut;
-        let x = aAr.pop();
-        let y = bAr.pop();
-        let sum = (x ? +x : 0) + (y ? +y : 0) + c;
-        console.log("sum:" + sum)
-        if (sum > 1) {
-            c = 1;
-            indOut = 0;
-
-        } else if (sum == 1) {
-            c = 0;
-            indOut = 1;
-        } else {
-            c = 0;
-            indOut = 0;
-        }
-        res = indOut + res;
+    let res = '';
+    while (i >= 0 || j >= 0) {
+        let sum = c + (a[i]?+a[i]:0) + (b[j]?+b[j]:0);
+        res = (sum % 2) + res;
+        c = Math.floor(sum / 2);
+        i--;
+        j--;
     }
-    if (c == 1) {
-        res = 1 + res;
-    }
+    if(c==1) return c+res;
     return res;
 };
+addBinary("11","1");
