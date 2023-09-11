b9e594de71aadf7476fc6b0792cb741f8105c79b
Amaldev <amaldev.psn@gmail.com>
2023-09-11 12:10:01 +0530

    c

--- algorithms/index.js
+++ algorithms/index.js
@@ -1,17 +1,15 @@
 function addBinary(a, b) {
-    debugger;
     let i = a.length - 1;
     let j = b.length - 1;
     let c = 0;
     let res = '';
     while (i >= 0 || j >= 0) {
-        let sum = c + (a[i]?+a[i]:0) + (b[j]?+b[j]:0);
+        let sum = c + (a[i] ? +a[i] : 0) + (b[j] ? +b[j] : 0);
         res = (sum % 2) + res;
         c = Math.floor(sum / 2);
         i--;
         j--;
     }
-    if(c==1) return c+res;
+    if (c == 1) return c + res;
     return res;
 };
-addBinary("11","1");
