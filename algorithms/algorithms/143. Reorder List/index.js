0aa74ad0dd0fcb9f8afca239da1772b5e4790228
Amaldev <amaldev.psn@gmail.com>
2023-09-14 00:45:48 +0530

    c

--- algorithms/algorithms/143. Reorder List/index.js
+++ algorithms/algorithms/143. Reorder List/index.js
@@ -14,5 +14,28 @@
  Do not return anything, modify head in-place instead.
  */
 function reorderList(head: ListNode | null): void {
-    #
+    let mid = middleNode(head);
+    let rev = reverseList(mid.next);
+
+
+};
+function middleNode(head: ListNode | null): ListNode | null {
+    let slow = head;
+    let fast = head;
+    while (slow?.next && fast?.next?.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return (fast?.next == null) ? slow : slow?.next;
+}
+function reverseList(head: ListNode | null): ListNode | null {
+    let current = head;
+    let prev = null;
+    while(current!=null){
+        let temp = current.next;
+        current.next = prev;
+        prev = current;
+        current= temp;
+    }
+    return prev;
 };
