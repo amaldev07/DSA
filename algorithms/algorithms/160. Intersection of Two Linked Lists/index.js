1e363453c7593c384e05965009c1a99cf5567a69
Amaldev <amaldev.psn@gmail.com>
2023-09-15 00:58:03 +0530

    c

--- algorithms/algorithms/160. Intersection of Two Linked Lists/index.js
+++ algorithms/algorithms/160. Intersection of Two Linked Lists/index.js
@@ -10,16 +10,33 @@
  * }
  */
 
+/* function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
+  let hm = new Map();
+  while (headA) {
+    hm.set(headA, 1);
+    headA = headA.next;
+  }
+  while (headB) {
+    if (hm.get(headB)) {
+      return headB;
+    }
+    headB = headB.next;
+  }
+}; */
 function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
-    let hm = new Map();
-    while (headA) {
-        hm.set(headA, 1);
-        headA = headA.next;
-    }
-    while (headB) {
-        if (hm.get(headB)) {
-            return headB;
+    let p1 = headA;
+    let p2 = headB;
+    while (p1 != p2) {
+        if (p1 == null) {
+            p1 = headB
+        } else {
+            p1 = p1.next;
         }
-        headB = headB.next;
+        if (p2 == null) {
+            p2 = headA
+        } else {
+            p2 = p2.next;
+        }
     }
+    return p1;
 };
