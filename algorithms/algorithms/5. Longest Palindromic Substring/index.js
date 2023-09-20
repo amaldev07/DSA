f3c7fd4c481cb57379424f346eb0888247074631
Amal <amaldev.psn@gmail.com>
2023-09-21 02:23:32 +0530

    c

--- algorithms/algorithms/5. Longest Palindromic Substring/index.js
+++ algorithms/algorithms/5. Longest Palindromic Substring/index.js
@@ -1,3 +1,30 @@
-function longestPalindrome(s: string): string {
+function longestPalindrome(s) {
+    let maxLen = 1;
+    let maxStr = s[0];
+    for (let i = 0; i < s.length; i++) {
+        let a = i;
+        let b = i;
+        while (a >= 0 && b <= (s.length - 1) && s[a] == s[b]) {
+            let len = b - a + 1;
+            if (len > maxLen) {
+                maxLen = len;
+                maxStr = s.substring(a, b + 1);
+            }
+            a = a - 1;
+            b = b + 1;
+        }
 
+        a = i;
+        b = i + 1;
+        while (a >= 0 && b <= (s.length - 1) && s[a] == s[b]) {
+            let len = b - a + 1;
+            if (len > maxLen) {
+                maxLen = len;
+                maxStr = s.substring(a, b + 1);
+            }
+            a = a - 1;
+            b = b + 1;
+        }
+    }
+    return maxStr;
 };
