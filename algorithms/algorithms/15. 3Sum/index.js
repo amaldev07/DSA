2ff704d2c8120887b3768b2032eeb49293cec6fd
Amaldev <amaldev.psn@gmail.com>
2023-09-25 03:18:45 +0530

    c

--- algorithms/algorithms/15. 3Sum/index.js
+++ algorithms/algorithms/15. 3Sum/index.js
@@ -1,13 +1,15 @@
 function threeSum(nums: number[]): number[][] {
     let res = [];
     for (let i = 0; i < nums.length - 2; i++) {
-        while (i > 0 && (nums[i] == nums[i - 1])) i++;
+        while (i > 0 && (nums[i] == nums[i - 1])) {
+            i++
+        };
         let j = i + 1;
         let k = nums.length - 1;
         while (j < k) {
             let a = nums[i];
-            let b = nums[i];
-            let c = nums[i];
+            let b = nums[j];
+            let c = nums[k];
             if ((a + b + c) > 0) {
                 k = k - 1;
             } else if ((a + b + c) < 0) {
@@ -16,8 +18,12 @@ function threeSum(nums: number[]): number[][] {
                 res.push([a, b, c]);
                 j = j + 1;
                 k = k - 1;
-                while (a[j] == a[j - 1]) j++;
-                while (a[k] == a[k + 1]) k--;
+                while (nums[j] == nums[j - 1]) {
+                    j++;
+                };
+                while (nums[k] == nums[k + 1]) {
+                    k--;
+                };
             }
         }
     }
