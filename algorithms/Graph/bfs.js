2424bd9af2aa649b8b946555524b3cdf27b0dcf9
Amal <amaldev.psn@gmail.com>
2023-10-14 02:17:56 +0530

    graph bfs completed

--- algorithms/Graph/bfs.js
+++ algorithms/Graph/bfs.js
@@ -12,10 +12,22 @@ let adjacencyList = {
 /* where v is the no of nodes */
 function bfs(v, adjacencyList) {
     let visited = new Array(v + 1).fill(0);
+    visited[1] = 1;
     let q = [1];
     let bfsOut = [];
     while (q.length > 0) {
-        
+        debugger;
+        let el = q.shift();
+        bfsOut.push(el);
+        let adjEl = adjacencyList[el];
+        for (let i = 0; i < adjEl.length; i++) {
+            if (visited[adjEl[i]] !== 1) {
+                visited[adjEl[i]] = 1;
+                q.push(adjEl[i]);
+            }
+        }
     }
     return bfsOut;
 }
+bfs(8, adjacencyList);
+/* Output ) [1, 2, 6, 3, 4, 7, 8, 5] */
