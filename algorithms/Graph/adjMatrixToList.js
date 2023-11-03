84669d3b65ec8abed92d543a4506dab7e50ce1c6
Amal <amaldev.psn@gmail.com>
2023-11-03 20:12:20 +0530

    c

--- algorithms/Graph/adjMatrixToList.js
+++ algorithms/Graph/adjMatrixToList.js
@@ -19,16 +19,18 @@ matrixToAdjacencyList([[1, 1, 0], [1, 1, 0], [0, 0, 1]]);
 you may use the below one with some minimul changes to the above one */
 /* Adjacency Matrix to Adjacency list  Conversion*/
 function matrixToAdjacencyList(matrix) {
-    const adjacencyList = {};
-    for (let i = 0; i < matrix.length; i++) {
-        adjacencyList[i + 1] = [];
-        for (let j = 0; j < matrix[i].length; j++) {
-            if (i != j && matrix[i][j] == 1) {
-                adjacencyList[i + 1].push(j + 1);
+    let n = matrix.length;
+    const adjacencyList = []
+    debugger;
+    for (let i = 0; i < n; i++) {
+        adjacencyList[i] = [];
+        for (let j = 0; j < n; j++) {
+            if ((i != j) && (matrix[i][j] == 1)) {
+                adjacencyList[i].push(j);
             }
         }
     }
     return adjacencyList;
 }
 /* Input [[0, 1, 1], [1, 0, 1], [1, 1, 0]]; */
-matrixToAdjacencyList([[1, 1, 0], [1, 1, 0], [0, 0, 1]]);
+matrixToAdjacencyList([[0, 1, 1], [1, 0, 1], [1, 1, 0]]);
