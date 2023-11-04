58ba5a54bcbb08f571ff0fc74c72b74d98f01cc1
Amal <amaldev.psn@gmail.com>
2023-11-04 20:12:20 +0530

    c

--- algorithms/Graph/547. Number of Provinces/index.js
+++ algorithms/Graph/547. Number of Provinces/index.js
@@ -1,3 +1,17 @@
-function findCircleNum(isConnected) { 
-
+function findCircleNum(isConnected) {
+    let n = isConnected.length;
+    let adjList = matrixToAdjacencyList(isConnected, n);
+    let vis = new Array(n).fill(0);
 };
+function matrixToAdjacencyList(matrix, n) {
+    const adjacencyList = []
+    for (let i = 0; i < n; i++) {
+        adjacencyList[i] = [];
+        for (let j = 0; j < n; j++) {
+            if ((i != j) && (matrix[i][j] == 1)) {
+                adjacencyList[i].push(j);
+            }
+        }
+    }
+    return adjacencyList;
+}
