06b4b673723521396c7769aecca1b3e12241ffb0
Amal <amaldev.psn@gmail.com>
2023-11-05 20:12:20 +0530

    Cc

--- custom_git_commands.sh
+++ custom_git_commands.sh
@@ -4,8 +4,9 @@ function customGitCommands() {
 
   # Run a series of Git commands
   git pull
-  git add .
-  git commit -m "Custom commit message"
+  git add -A
+  git commit -m "Cc"
+  git commit --amend --date "2023-11-05 20:12:20"
   git push
 }
 customGitCommands
