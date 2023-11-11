133b329c08cc1e85a954b88db2af2ffdd68e78ae
Amal <amaldev.psn@gmail.com>
2023-11-11 20:12:20 +0530

    Cc

--- algorithms/Graph/547. Number of Provinces/index.js
+++ algorithms/Graph/547. Number of Provinces/index.js
@@ -2,7 +2,26 @@ function findCircleNum(isConnected) {
     let n = isConnected.length;
     let adjList = matrixToAdjacencyList(isConnected, n);
     let vis = new Array(n).fill(0);
+    let count = 0;
+    for (let i = 0; i < n; i++) {
+        if (vis[i] !== 1) {
+            count = count + 1;
+            dfs(1, adjList, vis);
+        }
+    }
+    return count;
 };
+function dfs(node, adjacencyList, vis, out) {
+    debugger;
+    vis[node] = 1;
+    out.push(node);
+    let adjEl = adjacencyList[node];
+    for (let i = 0; i < adjEl.length; i++) {
+        if (vis[adjEl[i]] !== 1) {
+            dfs(adjEl[i], adjacencyList, vis, out);
+        }
+    }
+}
 function matrixToAdjacencyList(matrix, n) {
     const adjacencyList = []
     for (let i = 0; i < n; i++) {
