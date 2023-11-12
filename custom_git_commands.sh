3bc84c52b1bd5b9dafd30ce563793477b3d74caf
Amal <amaldev.psn@gmail.com>
2023-11-12 07:55:26 +0530

    Custom commit message

---
+++ custom_git_commands.sh
@@ -0,0 +1,11 @@
+function customGitCommands() {
+  # Navigate to your Git repository directory
+  # cd /path/to/your/repo
+
+  # Run a series of Git commands
+  git pull
+  git add .
+  git commit -m "Custom commit message"
+  git push
+}
+customGitCommands
