e314875400f22a4b2986de3f9de510545a1918a3
Amal <amaldev.psn@gmail.com>
2023-11-17 20:12:20 +0530

    c

---
+++ algorithms/algorithms/1108. Defanging an IP Address/index.js
@@ -0,0 +1,14 @@
+function defangIPaddr(address) {
+    /* 
+    // One liner solution using inbuilt string replaceAll()
+    return address.replaceAll('.', '[.]'); */
+    let dAddr = '';
+    for (let i = 0; i < address.length; i++) {
+        if (address[i] == '.') {
+            dAddr = dAddr + '[.]';
+        } else {
+            dAddr = dAddr + address[i];
+        }
+    }
+    return dAddr;
+};
