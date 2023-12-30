6b6277509db35856d7f546bae51f3ce12a34e97f
Amaldev <amaldev.psn@gmail.com>
2023-12-30 11:12:20 +0530

    c

--- algorithms/Graph/733. Flood Fill/index.js
+++ algorithms/Graph/733. Flood Fill/index.js
@@ -1,9 +1,32 @@
 function floodFill(image, sr, sc, color) {
-    let vis = getVisitedArray(image.length, image[0].length);
+    bfs(image, sr, sc, color);
+    return image;
 };
 
-function dfs() {
-
+// function bfs(v, adjacencyList) {
+function bfs(image, sr, sc, color) {
+    let n = image.length;
+    let m = image[0].length;
+    let vis = getVisitedArray(image.length, image[0].length);
+    let q = [];
+    q.push([sr, sc]);
+    vis[sr][sc] = 1;
+    image[sr][sc] = color;
+    while (q.length > 0) {
+        let el = q.pop();
+        image[el[0]][el[1]] = 1;
+        let items = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+        for (let i = 0; i < items.length; i++) {
+            let nr = sr + items[i][0];
+            let nc = sc + items[i][1];
+            if (vis[nr][nc] == 0 &&
+                nr >= 0 && nr < n &&
+                nc >= 0 && nc < m) {
+                vis[nr][nc] = 1;
+                q.push([nr, nc]);
+            }
+        }
+    }
 }
 function getVisitedArray(r, c) {
     let vis = [];
