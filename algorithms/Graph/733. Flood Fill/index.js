03998f63db7969d4cae4e47425efb059ce474f2d
Amaldev <amaldev.psn@gmail.com>
2023-12-31 16:10:39 +0530

    c

--- algorithms/Graph/733. Flood Fill/index.js
+++ algorithms/Graph/733. Flood Fill/index.js
@@ -1,3 +1,18 @@
 function floodFill(image, sr, sc, color) {
-
+    let vis = getVisitedArray(image.length, image[0].length);
 };
+
+function dfs() {
+
+}
+function getVisitedArray(r, c) {
+    let vis = [];
+    for (let i = 0; i < r; i++) {
+        let cVis = [];
+        for (let j = 0; j < c; j++) {
+            cVis.push(0);
+        }
+        vis.push(cVis);
+    }
+    return vis;
+}
