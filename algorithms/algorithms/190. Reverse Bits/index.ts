284714b946261d627ead3fabb4cf4a049265a4f3
Amaldev <amaldev.psn@gmail.com>
2024-01-02 01:35:43 +0530

    c

--- algorithms/algorithms/190. Reverse Bits/index.ts
+++ algorithms/algorithms/190. Reverse Bits/index.ts
@@ -1,8 +1,8 @@
 function reverseBits(n: number): number {
     let revSum = 0;
     for (let i = 0; i < 32; i++) {
-        let b = n & 1;
-        n = n >>> 1;
+        let b = n & 1; //to get the last digit
+        n = n >>> 1; // shifting n by one bit right, to change the last bit
         revSum = revSum + b * Math.pow(2, 31 - i);
     }
     return revSum;
