b3bc018d9c79042e3022af68b2915803647d1ee1
Amal <amaldev.psn@gmail.com>
2024-01-06 01:43:02 +0530

    c

---
+++ algorithms/Hashing/Print all pairs with given sum/index.js
@@ -0,0 +1,34 @@
+/*
+    Given an array of integers, and a number ‘sum’, print all pairs in the array 
+    whose sum is equal to ‘sum’.
+
+    Examples :
+    Input  :  arr[] = {1, 5, 7, -1, 5}, 
+            sum = 6
+    Output : (1, 5) (7, -1) (1, 5)
+    Input  :  arr[] = {2, 5, 17, -1}, 
+            sum = 7
+    Output :  (2, 5) 
+  ----------------------
+  Method 1
+  2 pointer
+  ------------------
+  let hm = new Map();
+  hm.set(1, 'val');
+  hm.get(1)
+
+  let hm =  new Map();
+  for(let i =0;i<arr.length;i++){
+    hm.set(arr[i], arr[i]);
+  }
+
+  let pairs = [];
+  for(let i =0;i<arr.length;i++){
+    let x = arr[i];
+    let y = sum =x;
+    
+  }
+
+  
+
+ */
