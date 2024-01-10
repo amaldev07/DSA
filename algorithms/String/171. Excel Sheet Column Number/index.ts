e3b69cf8f896f4e21318a35166b1a3084be1035e
Amaldev <amaldev.psn@gmail.com>
2024-01-11 03:17:52 +0530

    c

--- algorithms/String/171. Excel Sheet Column Number/index.ts
+++ algorithms/String/171. Excel Sheet Column Number/index.ts
@@ -1,3 +1,13 @@
+/* 
+Approach
+ABC= 26^21 + 26^12 + 26^03
+=> 26^2a + 26^1b + 26^0c
+
+where a,b,c means 1,2,3
+so it becomes
+for i=2 to 0
+ABC= 26^ia + 26^ib + 26^i*c
+*/
 function titleToNumber(columnTitle: string): number {
     let colNumber = 0;
     for (let i = columnTitle.length - 1; i >= 0; i--) {
