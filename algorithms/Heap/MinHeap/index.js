c14f72e453c4eeee36842e3e32145ffb2f83982a
Amaldev <amaldev.psn@gmail.com>
2024-03-02 23:50:32 +0530

    min heap

--- algorithms/Heap/MinHeap/index.js
+++ algorithms/Heap/MinHeap/index.js
@@ -1,1 +1,15 @@
- 
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+}
+
+getParentIndex(i){
+    return Math.floor((i - 1) / 2);
+}
+getLeftChildIndex(i){
+    return (i * 2) + 1;
+}
+getLeftChildIndex(i){
+    return (i * 2) + 2;
+}
