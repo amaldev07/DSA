dc30b630a3a1a2a81b338801261dce8076dcb50f
Amaldev <amaldev.psn@gmail.com>
2024-03-10 08:05:43 +0530

    MaxHeap

---
+++ algorithms/Heap/MaxHeap/index.js
@@ -0,0 +1,45 @@
+function MaxHeap() {
+    let heap = [];
+
+    function getParentIndex(i) {
+        return Math.floor((i - 1) / 2);
+    }
+
+    function getLeftChildIndex(i) {
+        return ((i * 2) + 1);
+    }
+
+    function getRightChildIndex(i) {
+        return ((i * 2) + 2);
+    }
+
+    function swap(index1, index2) {
+        [heap[index1], heap[index2]] = [heap[index2], heap[index1]];
+    }
+
+    function insert(val) {
+        heap.push(val);
+        heapifyUp();
+        console.log(heap);
+    }
+
+    function heapifyUp() {
+        let index = heap.length - 1;
+        while (index > 0) {
+            let parentIndex = getParentIndex(index);
+            if (heap[parentIndex] < heap[index]) {
+                swap(parentIndex, index);
+                index = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+    return { insert }
+}
+
+let maxHeap = new MaxHeap();
+maxHeap.insert(1);
+maxHeap.insert(2);
+maxHeap.insert(3);
+maxHeap.insert(4);
--- algorithms/Heap/MinHeap/index.js
+++ algorithms/Heap/MinHeap/index.js
@@ -69,7 +69,11 @@ minHeap.insert(10);
 minHeap.insert(9);
 minHeap.insert(8);
 minHeap.insert(7);
-minHeap.insert(7);
-minHeap.insert(7);
-minHeap.extractMin();
+minHeap.insert(6);
+minHeap.insert(5);
+minHeap.insert(4);
+minHeap.insert(3);
+minHeap.insert(2);
+minHeap.insert(1);
+// minHeap.extractMin();
 
