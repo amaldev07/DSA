edeee270a2e30977d89b88414ad5e81aae16f1bf
Amaldev <amaldev.psn@gmail.com>
2024-03-18 00:08:07 +0530

    solve

---
+++ algorithms/Graph/130. Surrounded Regions/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var solve = function(board) {
+    let rows = board.length;
+    let cols = board[0].length;
+    for(let i=0;i<cols;i++){
+        
+    }
+};
