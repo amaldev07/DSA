cdf69dc4666a607a1900d68046f13b5c3b7d768e
Amaldev <amaldev.psn@gmail.com>
2024-03-18 05:43:04 +0530

    numEnclaves

---
+++ algorithms/Graph/1020. Number of Enclaves/index.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var numEnclaves = function (grid) {
+    let rows = grid.length;
+    let cols = grid[0].length;
+    let vis = Array.from({ length: rows }, () => { Array(cols).fill(0) });
+    for (let i = 0; i < cols; i++) {
+        if ()
+    }
+};
