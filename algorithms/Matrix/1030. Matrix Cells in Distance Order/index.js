00c60097367c0f20f33518ffc94f88aaff560145
Amaldev <amaldev.psn@gmail.com>
2024-03-23 12:06:40 +0530

    allCellsDistOrder

---
+++ algorithms/Matrix/1030. Matrix Cells in Distance Order/index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} rows
+ * @param {number} cols
+ * @param {number} rCenter
+ * @param {number} cCenter
+ * @return {number[][]}
+ */
+var allCellsDistOrder = function(rows, cols, rCenter, cCenter) {
+    let out = [];
+    let q = [[rCenter, cCenter]];
+    let vis = Array({length:rows,})
+    while()
+    return out;
+};
