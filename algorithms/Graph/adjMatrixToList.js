ef30b741926663c73987b4bf13857a0b78fedbbc
Amaldev <amaldev.psn@gmail.com>
2024-10-05 10:09:59 +0530

    matrixToAdjacencyList

--- algorithms/Graph/adjMatrixToList.js
+++ algorithms/Graph/adjMatrixToList.js
@@ -1,36 +1,18 @@
-/* Adjacency Matrix to Adjacency list  Conversion*/
-function matrixToAdjacencyList(matrix) {
-    const adjacencyList = {};
-    for (let i = 0; i < matrix.length; i++) {
-        adjacencyList[i] = [];
-        for (let j = 0; j < matrix[i].length; j++) {
-            if (matrix[i][j] == 1) {
-                adjacencyList[i].push(j);
-            }
-        }
-    }
-    return adjacencyList;
-}
-/* Input [[0, 1, 1], [1, 0, 1], [1, 1, 0]]; */
-matrixToAdjacencyList([[1, 1, 0], [1, 1, 0], [0, 0, 1]]);
-/* ------------------------------------------------------------------------------------------------- */
 
-/* For the Leetcode question 
-you may use the below one with some minimul changes to the above one */
-/* Adjacency Matrix to Adjacency list  Conversion*/
 function matrixToAdjacencyList(matrix) {
+    debugger;
     let n = matrix.length;
-    const adjacencyList = []
+    let adjacencyList = {};
     for (let i = 0; i < n; i++) {
-        adjacencyList[i] = [];
+        adjacencyList[i + 1] = [];
         for (let j = 0; j < n; j++) {
-            if ((i != j) && (matrix[i][j] == 1)) {
-                adjacencyList[i].push(j);
+            if ((i != j) && matrix[i][j] == 1) {
+                adjacencyList[i+1].push(j+1);
             }
         }
     }
     return adjacencyList;
 }
 /* Input [[0, 1, 1], [1, 0, 1], [1, 1, 0]]; */
-matrixToAdjacencyList([[0, 1, 1], [1, 0, 1], [1, 1, 0]]);
---------------------------------------------------------------------------
+// matrixToAdjacencyList([[0, 1, 1], [1, 0, 1], [1, 1, 0]]);
+matrixToAdjacencyList([[1, 1, 0], [1, 1, 0], [0, 0, 1]]);
