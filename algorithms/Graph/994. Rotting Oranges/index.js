c1f011582df6b1a90fdc919df88286af98b4089c
Amaldev <amaldev.psn@gmail.com>
2024-10-29 13:48:59 +0530

    c

--- algorithms/Graph/994. Rotting Oranges/index.js
+++ algorithms/Graph/994. Rotting Oranges/index.js
@@ -3,6 +3,7 @@
  * @return {number}
  */
 var orangesRotting = function (grid) {
+    debugger;
     let m = grid.length;
     let n = grid[0].length;
     let q = [];
@@ -13,40 +14,44 @@ var orangesRotting = function (grid) {
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
             if (grid[i][j] == 2) {
-                let orange = grid[i][j];
-                q.push([orange, 0]);
+                q.push([[i, j], 0]);
                 vis[i][j] = 2;
 
-            } else if (grid[i][j]) {
+            } else if (grid[i][j] == 1) {
                 count = count + 1;
             }
         }
     }
+    if (count == 0) return 0;
     while (q.length > 0) {
         let ele = q.shift();
         let r = ele[0][0];
         let c = ele[0][1];
         let neighbours = [
-            [r - 1, c]
-            [r + 1, c]
-            [r, c - 1]
+            [r - 1, c],
+            [r + 1, c],
+            [r, c - 1],
             [r, c + 1]
         ]
         for (let i = 0; i < neighbours.length; i++) {
             let e = neighbours[i];
             let x = e[0];
-            let y = e[y];
+            let y = e[1];
             if (x >= 0 && x < m &&
                 y >= 0 && y < n &&
                 vis[x][y] != 2 &&
                 grid[x][y] == 1
             ) {
-                q.push([neighbours[i], ele[1] + 1])
+                time = ele[1]+1;
+                q.push([neighbours[i], time]);
+                vis[x][y] = 2;
                 rottonCount = rottonCount + 1;
-                time = ele[1];
             }
         }
     }
-    if (rottonCount != count) return false;
+    if (rottonCount != count) return -1;
     return time;
 };
+
+let grid = [[2, 1, 1], [1, 1, 0], [0, 1, 1]];
+orangesRotting(grid);
