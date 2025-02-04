77276047032f5bf1d63b42fb4efe9e4a8ceb79b3
Amaldev <amaldev.psn@gmail.com>
2025-02-04 08:46:30 +0530

    maxAscendingSum

---
+++ algorithms/Array/1800. Maximum Ascending Subarray Sum/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAscendingSum = function (nums) {
+    let maxSum = nums[0];
+    let sum = nums[0];
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] < nums[i + 1]) {
+            sum = sum + nums[i + 1]
+        } else {
+            sum = nums[i + 1];
+        }
+        maxSum = Math.max(sum, maxSum);
+    }
+    return maxSum;
+};
