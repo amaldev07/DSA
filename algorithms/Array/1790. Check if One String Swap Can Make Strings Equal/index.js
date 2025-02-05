dc401661ddc3f966ec3d1669162f61aa7b7a8023
Amaldev <amaldev.psn@gmail.com>
2025-02-05 09:57:30 +0530

    areAlmostEqual

---
+++ algorithms/Array/1790. Check if One String Swap Can Make Strings Equal/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var areAlmostEqual = function (s1, s2) {
+    if (s1 == s2) return true;
+    let diff = [];
+    for (let i = 0; i < s1.length; i++) {
+        if (s1[i] != s2[i]) {
+            diff.push(i);
+        }
+    }
+    if (diff.length == 0) return true;
+    if (diff.length == 2) {
+        const [i, j] = diff;
+        if (s1[i] == s2[j] && s1[j] == s2[i]) return true;
+    }
+    return false;
+};
