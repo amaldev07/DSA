efe841e7e2e4b04ed0fdfdd3f48485f94dbfd372
Amaldev <amaldev.psn@gmail.com>
2025-02-07 11:07:14 +0530

    queryResults

--- algorithms/Hashing/3160. Find the Number of Distinct Colors Among the Balls/index.js
+++ algorithms/Hashing/3160. Find the Number of Distinct Colors Among the Balls/index.js
@@ -39,22 +39,16 @@ var queryResults = function (limit, queries) {
     let res = [];
     for (let i = 0; i < queries.length; i++) {
         let [ball, color] = queries[i];
-        if (map.get(ball) == undefined) {
-            map.set(ball, color);
-            set.add(color);
-            colorCOunt.set(color, (colorCOunt.get(color) || 0) + 1);
-        } else {
+        if (map.has(ball)) {
             let previousColor = map.get(ball);
-            if (colorCOunt.get(previousColor) != undefined &&
-                colorCOunt.get(previousColor) <= 1) {
+            if (colorCOunt.get(previousColor) <= 1) {
                 set.delete(previousColor);
             }
             colorCOunt.set(previousColor, colorCOunt.get(previousColor) - 1);
-            colorCOunt.set(color, (colorCOunt.get(color) || 0) + 1);
-            
-            map.set(ball, color);
-            set.add(color);
         }
+        colorCOunt.set(color, (colorCOunt.get(color) || 0) + 1);
+        map.set(ball, color);
+        set.add(color);
         res.push(set.size);
     }
     return res;
