ac265bbbd0083d63252ef4fec871e046bcefb5be
Amaldev <amaldev.psn@gmail.com>
2025-02-07 11:04:04 +0530

    c

---
+++ algorithms/Hashing/3160. Find the Number of Distinct Colors Among the Balls/index.js
@@ -0,0 +1,61 @@
+/*
+var queryResults = function (limit, queries) {
+   let map = new Map();
+   let colorCOunt = new Map();
+   let set = new Set();
+   let res = [];
+   for (let i = 0; i < queries.length; i++) {
+       let [ball, color] = queries[i];
+       if (map.get(ball) == undefined) {
+           map.set(ball, color);
+           set.add(color);
+           colorCOunt.set(color, (colorCOunt.get(color) || 0) + 1);
+       } else {
+           let previousColor = map.get(ball);
+           if (colorCOunt.get(previousColor) != undefined &&
+               colorCOunt.get(previousColor) <= 1) {
+               set.delete(previousColor);
+           }
+           colorCOunt.set(previousColor, colorCOunt.get(previousColor) - 1);
+           colorCOunt.set(color, (colorCOunt.get(color) || 0) + 1);
+           
+           map.set(ball, color);
+           set.add(color);
+       }
+       res.push(set.size);
+   }
+   return res;
+}; */
+
+/**
+ * @param {number} limit
+ * @param {number[][]} queries
+ * @return {number[]}
+ */
+var queryResults = function (limit, queries) {
+    let map = new Map();
+    let colorCOunt = new Map();
+    let set = new Set();
+    let res = [];
+    for (let i = 0; i < queries.length; i++) {
+        let [ball, color] = queries[i];
+        if (map.get(ball) == undefined) {
+            map.set(ball, color);
+            set.add(color);
+            colorCOunt.set(color, (colorCOunt.get(color) || 0) + 1);
+        } else {
+            let previousColor = map.get(ball);
+            if (colorCOunt.get(previousColor) != undefined &&
+                colorCOunt.get(previousColor) <= 1) {
+                set.delete(previousColor);
+            }
+            colorCOunt.set(previousColor, colorCOunt.get(previousColor) - 1);
+            colorCOunt.set(color, (colorCOunt.get(color) || 0) + 1);
+            
+            map.set(ball, color);
+            set.add(color);
+        }
+        res.push(set.size);
+    }
+    return res;
+};
