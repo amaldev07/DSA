6850ea304d9a86d90fdb9f9f5bab438483617cee
Amaldev <amaldev.psn@gmail.com>
2025-02-15 00:15:35 +0530

    c

--- algorithms/binary search/441. Arranging Coins/index.js
+++ algorithms/binary search/441. Arranging Coins/index.js
@@ -1,1 +1,13 @@
-// functions
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoins = function (n) {
+    let c = 0;
+    while (n > 0) {
+        c = c + 1;
+        n = n - c;
+        if (n < 0) c = c - 1;
+    }
+    return c;
+};
