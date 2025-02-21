063dccab82a828bde00579622b3db87a3e009cfc
Amaldev <amaldev.psn@gmail.com>
2025-02-21 23:22:05 +0530

    heapifyDown

--- algorithms/Heap/MaxHeap/index.js
+++ algorithms/Heap/MaxHeap/index.js
@@ -27,6 +27,7 @@ function MaxHeap() {
         let max = heap[0];
         heap[0] = heap.pop();
         heapifyDown();
+        console.log(heap);
         return max;
     }
 
@@ -35,7 +36,7 @@ function MaxHeap() {
         let length = heap.length;
         while (getLeftChildIndex(index) < length) {
             let maxValIndex = getLeftChildIndex(index);
-            if (getRightChildIndex(index) < length && getRightChildIndex(index) > getLeftChildIndex(index)) {
+            if (getRightChildIndex(index) < length && heap[getRightChildIndex(index)] > heap[getLeftChildIndex(index)]) {
                 maxValIndex = getRightChildIndex(index);
             }
             if (heap[index] < heap[maxValIndex]) {
@@ -67,3 +68,7 @@ maxHeap.insert(1);
 maxHeap.insert(2);
 maxHeap.insert(3);
 maxHeap.insert(4);
+maxHeap.insert(5);
+maxHeap.insert(6);
+maxHeap.insert(7);
+maxHeap.insert(8);
