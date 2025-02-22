b22b5339d6c67953f03dd526bf9a1bb6e17fe405
Amaldev <amaldev.psn@gmail.com>
2025-02-22 11:54:21 +0530

    findKthLargest

--- algorithms/Heap/215. Kth Largest Element in an Array/index.js
+++ algorithms/Heap/215. Kth Largest Element in an Array/index.js
@@ -14,24 +14,20 @@ var findKthLargest = function (nums, k) {
     }
     return maxVal;
 };
-
-function MaxHeap() {
+function MinHeap() {
     let heap = [];
-
     function getParentIndex(i) {
         return Math.floor((i - 1) / 2);
     }
-
     function getLeftChildIndex(i) {
         return ((i * 2) + 1);
     }
-
     function getRightChildIndex(i) {
         return ((i * 2) + 2);
     }
 
-    function swap(index1, index2) {
-        [heap[index1], heap[index2]] = [heap[index2], heap[index1]];
+    function swap(parentIndex, index) {
+        [heap[parentIndex], heap[index]] = [heap[index], heap[parentIndex]];
     }
 
     function insert(val) {
@@ -40,42 +36,48 @@ function MaxHeap() {
         console.log(heap);
     }
 
-    function extractMax() {
-        let max = heap[0];
+    function extractMin() {
+        if (heap.length == 0) return null;
+        if (heap.length == 1) return heap.pop();
+        let min = heap[0];
         heap[0] = heap.pop();
         heapifyDown();
         console.log(heap);
-        return max;
-    }
-
-    function heapifyDown() {
-        let index = 0;
-        let length = heap.length;
-        while (getLeftChildIndex(index) < length) {
-            let maxValIndex = getLeftChildIndex(index);
-            if (getRightChildIndex(index) < length && heap[getRightChildIndex(index)] > heap[getLeftChildIndex(index)]) {
-                maxValIndex = getRightChildIndex(index);
-            }
-            if (heap[index] < heap[maxValIndex]) {
-                swap(index, maxValIndex);
-                index = maxValIndex;
-            } else {
-                break;
-            }
-        }
+        return min;
     }
 
     function heapifyUp() {
         let index = heap.length - 1;
         while (index > 0) {
             let parentIndex = getParentIndex(index);
-            if (heap[parentIndex] < heap[index]) {
+            if (heap[parentIndex] > heap[index]) {
                 swap(parentIndex, index);
                 index = parentIndex;
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    function heapifyDown() {
+        let index = 0;
+        while (getLeftChildIndex(index) <= (heap.length - 1)) {
+            let leftIndex = getLeftChildIndex(index);
+            let rightIndex = getRightChildIndex(index);
+            let minValueIndex = leftIndex;
+            if (rightIndex <= heap.length - 1 && heap[rightIndex] < heap[leftIndex]) {
+                minValueIndex = rightIndex;
+            }
+            if (heap[index] > heap[minValueIndex]) {
+                swap(index, minValueIndex);
+                index = minValueIndex;
             } else {
                 break;
             }
         }
     }
-    return { insert, extractMax }
-};
+    return { insert, extractMin };
+}
+
+
