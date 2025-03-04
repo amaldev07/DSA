795af31b8cdd68087d80b0bf14bbaed5aca06e26
Amaldev <amaldev.psn@gmail.com>
2025-03-04 10:18:58 +0530

    allCellsDistOrder

--- algorithms/Matrix/1030. Matrix Cells in Distance Order/index.js
+++ algorithms/Matrix/1030. Matrix Cells in Distance Order/index.js
@@ -5,10 +5,28 @@
  * @param {number} cCenter
  * @return {number[][]}
  */
-var allCellsDistOrder = function(rows, cols, rCenter, cCenter) {
-    let out = [];
+var allCellsDistOrder = function (rows, cols, rCenter, cCenter) {
+    let out = [[rCenter, cCenter]];
+    let vis = Array.from({ length: rows }, () => Array(cols).fill(0));
     let q = [[rCenter, cCenter]];
-    let vis = Array({length:rows,})
-    while()
+    vis[rCenter][cCenter] = 1;
+    while (q.length > 0) {
+        let el = q.shift();
+        let delr = [-1, 0, 1, 0];
+        let delc = [0, 1, 0, -1];
+
+        for (let i = 0; i < 4; i++) {
+            let nr = el[0] + delr[i];
+            let nc = el[1] + delc[i]
+            if (nr >= 0 && nr < rows &&
+                nc >= 0 && nc < cols &&
+                vis[nr][nc] != 1
+            ) {
+                vis[nr][nc] = 1;
+                q.push([nr, nc]);
+                out.push([nr, nc]);
+            }
+        }
+    }
     return out;
 };
