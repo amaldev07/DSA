3336cc39017168dfd286eaf11095e1b8434c7c23
Amaldev <amaldev.psn@gmail.com>
2025-03-09 11:47:03 +0530

    isBipartite

--- algorithms/Graph/785. Is Graph Bipartite/index.js
+++ algorithms/Graph/785. Is Graph Bipartite/index.js
@@ -3,12 +3,26 @@
  * @return {boolean}
  */
 var isBipartite = function (graph) {
-    let q = [];
     let vis = [];
+
     for (let i = 0; i < graph.length; i++) {
         vis[i] = -1;
     }
-    q.push(0);
+    for (let i = 0; i < graph.length; i++) {
+        if (vis[i] == -1) {
+            if (check(graph, vis, i) == false) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+var check = function (graph, vis, start) {
+    let q = [];
+
+    q.push(start);
+    vis[start] = 0;
     while (q.length > 0) {
         let n = q.shift();
         let color = vis[n];
@@ -25,5 +39,6 @@ var isBipartite = function (graph) {
     }
     return true;
 };
-let graph = [[1, 2, 3], [0, 2], [0, 1, 3], [0, 2]]
-isBipartite(graph);
+// [[], [2, 4, 6], [1, 4, 8, 9], [7, 8], [1, 2, 8, 9], [6, 9], [1, 5, 7, 8, 9], [3, 6, 9], [2, 3, 4, 6, 9], [2, 4, 5, 6, 7, 8]]
+// let graph1 = [[1, 2, 3], [0, 2], [0, 1, 3], [0, 2]]
+// isBipartite(graph);
