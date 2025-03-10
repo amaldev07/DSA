9e775020bb2ea7bf604f62c897a7b75ae4c1f0f2
Amaldev <amaldev.psn@gmail.com>
2025-03-10 10:20:45 +0530

    cc

--- algorithms/Graph/695. Max Area of Island/index.js
+++ algorithms/Graph/695. Max Area of Island/index.js
@@ -5,12 +5,14 @@
 var maxAreaOfIsland = function (grid) {
     let rows = grid.length;
     let cols = grid[0].length;
-    let vis = Array({ length: rows }, () => Array(cols).fill(0));
+    let vis = Array.from({ length: rows }, () => Array(cols).fill(0));
     let maxArea = [0];
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
-            dfs(i, j, vis, grid, maxArea, 1);
-            vis[i][j] = 1;
+            if (vis[i][j] == 0 && grid[i][j] ==1) {
+                vis[i][j] = 1;
+                dfs(i, j, vis, grid, maxArea, 1);
+            }
         }
     }
     return maxArea[0];
@@ -18,17 +20,20 @@ var maxAreaOfIsland = function (grid) {
 
 function dfs(r, c, vis, grid, maxArea, count) {
     maxArea[0] = maxArea[0] > count ? maxArea[0] : count;
-    let totalRows = board.length;
-    let totalCols = board[0].length;
+    let totalRows = grid.length;
+    let totalCols = grid[0].length;
     let delr = [-1, 0, 1, 0];
     let delc = [0, 1, 0, -1];
     for (let i = 0; i < 4; i++) {
         let nr = r + delr[i];
         let nc = c + delc[i];
         if (nr >= 0 && nr < totalRows && nc >= 0 && nc < totalCols &&
-            grid[nr][nc] == 1 && vis[nr][nc] != 0) {
+            grid[nr][nc] == 1 && vis[nr][nc] == 0) {
             vis[nr][nc] = 1;
             dfs(nr, nc, vis, grid, maxArea, count + 1);
-        }
+        } 
     }
 }
+
+let grid = [[0,0,0,0,0,0,0,0]];
+maxAreaOfIsland(grid);
