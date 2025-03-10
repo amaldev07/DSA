fc1921636c9bcd6670480482fcd32e32f1f456c5
Amaldev <amaldev.psn@gmail.com>
2025-03-10 10:31:56 +0530

    maxAreaOfIsland

--- algorithms/Graph/695. Max Area of Island/index.js
+++ algorithms/Graph/695. Max Area of Island/index.js
@@ -9,9 +9,9 @@ var maxAreaOfIsland = function (grid) {
     let maxArea = [0];
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
-            if (vis[i][j] == 0 && grid[i][j] ==1) {
+            if (vis[i][j] == 0 && grid[i][j] == 1) {
                 vis[i][j] = 1;
-                dfs(i, j, vis, grid, maxArea, 1);
+                dfs(i, j, vis, grid, maxArea, [1]);
             }
         }
     }
@@ -19,21 +19,22 @@ var maxAreaOfIsland = function (grid) {
 };
 
 function dfs(r, c, vis, grid, maxArea, count) {
-    maxArea[0] = maxArea[0] > count ? maxArea[0] : count;
+    maxArea[0] = maxArea[0] > count[0] ? maxArea[0] : count[0];
     let totalRows = grid.length;
     let totalCols = grid[0].length;
     let delr = [-1, 0, 1, 0];
     let delc = [0, 1, 0, -1];
+
     for (let i = 0; i < 4; i++) {
         let nr = r + delr[i];
         let nc = c + delc[i];
         if (nr >= 0 && nr < totalRows && nc >= 0 && nc < totalCols &&
             grid[nr][nc] == 1 && vis[nr][nc] == 0) {
             vis[nr][nc] = 1;
-            dfs(nr, nc, vis, grid, maxArea, count + 1);
-        } 
+            count[0] = count[0] + 1;
+            dfs(nr, nc, vis, grid, maxArea, count);
+        }
     }
 }
-
-let grid = [[0,0,0,0,0,0,0,0]];
-maxAreaOfIsland(grid);
+/* let grid = [[1, 1, 1], [1, 0, 0]];
+maxAreaOfIsland(grid); */
