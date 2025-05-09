b433c21a5cdd67ec23d8401be233ef9e5f2d1de5
Amaldev <amaldev.psn@gmail.com>
2025-05-09 10:32:03 +0530

    ok

--- algorithms/Hashing/1399. Count Largest Group/index.js
+++ algorithms/Hashing/1399. Count Largest Group/index.js
@@ -3,9 +3,27 @@
  * @return {number}
  */
 var countLargestGroup = function (n) {
+        let map = new Map();
+        let sumCountMax = 0;
+        debugger;
         for (let i = 1; i <= n; i++) {
                 let sum = getDigitSum(i);
+                if (map.has(sum)) {
+                        let count = map.get(sum) + 1;
+                        map.set(sum, count);
+                        Math.max(sumCountMax, count);
+                } else {
+                        map.set(sum, 1);
+                        Math.max(sumCountMax, 1);
+                }
         }
+        let maxSumCount = 0;
+        map.forEach((count, sum) => {
+                if (count == sumCountMax) {
+                        maxSumCount = maxSumCount + 1;
+                }
+        });
+        return maxSumCount;
 };
 function getDigitSum(i) {
         let s = 0;
@@ -15,3 +33,4 @@ function getDigitSum(i) {
         }
         return s;
 }
+countLargestGroup(13);
