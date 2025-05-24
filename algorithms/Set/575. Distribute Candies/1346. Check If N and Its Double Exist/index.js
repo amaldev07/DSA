fd7aef710f031a17f79f6be907f9de29a10df809
Amaldev <amaldev.psn@gmail.com>
2025-05-24 14:15:09 +0530

    update 'algorithms/Set/575. Distribute Candies/1346. Check If N and Its Double Exist/index.js'

--- algorithms/Set/575. Distribute Candies/1346. Check If N and Its Double Exist/index.js
+++ algorithms/Set/575. Distribute Candies/1346. Check If N and Its Double Exist/index.js
@@ -2,6 +2,15 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-var checkIfExist = function(arr) {
-    
+var checkIfExist = function (arr) {
+    let set = new Set();
+    for (let i = 0; i < arr.length; i++) {
+        let el = arr[i];
+        if (set.has(el * 2) || (el % 2 === 0 && set.has(el / 2))) {
+            return true;
+        } else {
+            set.add(el);
+        }
+    }
+    return false;
 };
