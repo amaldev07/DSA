9b6373303ea9720ce3823675d23646e2b2eff132
Amaldev <amaldev.psn@gmail.com>
2026-05-29 00:15:21 +0530

    feat: create largestComponent.js

---
+++ algorithms/Graph/Learning From GPT/largestComponent.js
@@ -0,0 +1,22 @@
+function getMaxSize(graph) {
+    let visited = new Set();
+    function dfs(node) {
+        if (visited.has(node)) return 0;
+        visited.add(node);
+        let count = 1;
+        for (let neighbour in graph[node]) {
+            let countTilleighbourNode = dfs(neighbour);
+            count = count + countTilleighbourNode;
+        }
+        return count;
+    }
+
+    let maxLength = 0;
+    for (let i = 0; i < graph.length; i++) {
+        if (!visited.has(i)) {
+            let size = dfs(i);
+            maxLength = Math.max(size, maxLength);
+        }
+    }
+    return maxLength;
+}
