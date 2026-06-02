685fcd01f3d560d981c7a4dd651dd0d247858ab4
Amaldev <amaldev.psn@gmail.com>
2026-06-02 23:54:41 +0530

    MinHeap

--- algorithms/Graph/Learning From GPT/Min Heap/index.js
+++ algorithms/Graph/Learning From GPT/Min Heap/index.js
@@ -18,6 +18,35 @@ class MinHeap {
             index = parentIndex;
         }
     }
+    removeMin() {
+        if (this.heap.length == 0) return null;
+        let min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        let index = 0;
+        while (true) {
+            let smallest = index;
+            let leftIndex = (index * 2) + 1;
+            let rightIndex = (index * 2) + 2;
+            if (leftIndex < this.heap.length && this.heap[leftIndex] < this.heap[smallest]) {
+                smallest = leftIndex;
+            }
+
+            if (rightIndex < this.heap.length && this.heap[rightIndex] < this.heap[smallest]) {
+                smallest = rightIndex;
+            }
+
+            if (smallest == index) {
+                break;
+            }
+            //swap
+            let temp = this.heap[index];
+            this.heap[index] = this.heap[smallest];
+            this.heap[smallest] = temp;
+
+            index = smallest;
+        }
+        return min;
+    }
     showHeap() {
         return this.heap;
     }
