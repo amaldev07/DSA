7903bda2f9a10e72f89efa138b60c6b84309103b
Amaldev <amaldev.psn@gmail.com>
2026-06-19 00:06:19 +0530

    kruskal

--- Learning From GPT/Graph/Kruskal/index.js
+++ Learning From GPT/Graph/Kruskal/index.js
@@ -1,61 +1,52 @@
 function kruskal(n, edges) {
-    // edges = [[u, v, weight], ...]
+    let totalCost = 0;
+    let selectedEdges = [];
     edges.sort((a, b) => a[2] - b[2]);
 
     let dsu = new DSU(n);
-    let totalCost = 0;
-    let selectedEdges = [];
-
-    for (let [u, v, weight] of edges) {
-        if (dsu.union(u, v)) {
-            totalCost += weight;
-            selectedEdges.push([u, v, weight]);
+    for (let [a, b, weight] of edges) {
+        if (dsu.union(a, b)) {
+            selectedEdges.push([a, b, weight]);
+            totalCost = totalCost + weight;
         }
-
         if (selectedEdges.length === n - 1) {
             break;
         }
     }
-
     return {
         totalCost,
         selectedEdges
-    };
+    }
 }
 
 class DSU {
     constructor(n) {
         this.parent = [];
         this.size = [];
-
         for (let i = 0; i < n; i++) {
             this.parent[i] = i;
             this.size[i] = 1;
         }
     }
-
     find(node) {
         if (this.parent[node] !== node) {
             this.parent[node] = this.find(this.parent[node]);
         }
-
         return this.parent[node];
     }
 
     union(a, b) {
-        let leaderA = this.find(a);
-        let leaderB = this.find(b);
-
-        if (leaderA === leaderB) {
-            return false; // cycle
+        let parentofA = this.find(a);
+        let parentofB = this.find(b);
+        if (parentofA === parentofB) {
+            return false;
         }
-
-        if (this.size[leaderA] < this.size[leaderB]) {
-            this.parent[leaderA] = leaderB;
-            this.size[leaderB] += this.size[leaderA];
+        if (this.size[parentofA] < this.size[parentofB]) {
+            this.parent[parentofA] = parentofB;
+            this.size[parentofB] = this.size[parentofB] + this.size[parentofA];
         } else {
-            this.parent[leaderB] = leaderA;
-            this.size[leaderA] += this.size[leaderB];
+            this.parent[parentofB] = parentofA;
+            this.size[parentofA] = this.size[parentofB] + this.size[parentofA];
         }
 
         return true;
